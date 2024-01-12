66c42f5990a42d108db9fee3bfd429587c7c9290
Adriana FERRARI <adrifok@gmail.com>
2024-01-12 20:45:33 -0300

    3rd commit test

--- src/addition.py
+++ src/addition.py
@@ -1,6 +1,8 @@
 # app.py
 # This is a test commit
-# This is a 2nd test commit
+# This is a 2nd test commit for hosted runner to run
+# This is a 3rd test commit for hosted runner to run
+
 def add(a, b):
     return a + b
 
